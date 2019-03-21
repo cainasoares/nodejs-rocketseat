57986ebdcb6e9cbb301ae05fd91b00d41ebcb8a7
Cainã Soares <caina.pandora@gmail.com>
2019-03-21 07:16:43 -0300

    sequelizer

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
 const path = require('path');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-nunjucks.configure('views', {
+nunjucks.configure(path.resolve('app', 'views'), {
   autoescape: true,
   express: app,
 });
 
 app.set('view engine', 'njk');
-app.set('views', path.join(__dirname, 'views'));
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
-});
-
-app.post('/auth', (req, res) => {
-  const { username, password } = req.body;
-
-  res.send(`Você logou com usuário ${username} e senha ${password}`);
+  // res.send('Olá');
 });
 
 app.listen(3000);
