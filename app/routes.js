0ecc0e7b8834ea2016334ef5f695112cdf21ec95
Cainã Soares <caina.pandora@gmail.com>
2019-03-24 00:39:03 -0300

    flash messages e sessão

--- app/routes.js
+++ app/routes.js
@@ -5,5 +5,8 @@ const routes = express.Router();
 const authController = require('./controllers/authController');
 
 routes.get('/', authController.signin);
+routes.get('/signup', authController.signup);
+
+routes.post('/register', authController.register);
 
 module.exports = routes;
