082b2c7cf98c25c1ea76a79e681a7dddac63b368
Cainã Soares <caina.pandora@gmail.com>
2019-03-23 23:05:17 -0300

    utilizando sequelizer e adicionando controller

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
 const path = require('path');
+const routes = require('./app/routes');
 
 const app = express();
 
@@ -13,9 +14,6 @@ nunjucks.configure(path.resolve('app', 'views'), {
 app.set('view engine', 'njk');
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.render('index');
-  // res.send('Olá');
-});
+app.use('/', routes);
 
 app.listen(3000);
