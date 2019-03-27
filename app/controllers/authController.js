28dc7d093315330a474ba72acc83e8748bd575fc
Cainã Soares <caina_anderson@hotmail.com>
2019-03-26 23:41:19 -0300

    persistindo sessão no banco

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -10,38 +10,52 @@ module.exports = {
     return res.render('auth/signup');
   },
 
-  async register(req, res) {
-    const { email } = req.body;
-    if (await User.findOne({ where: { email } })) {
-      req.flash('error', 'E-mail já cadastrado');
-      return res.redirect('back');
+  async register(req, res, next) {
+    try {
+      const { email } = req.body;
+      if (await User.findOne({ where: { email } })) {
+        req.flash('error', 'E-mail já cadastrado');
+        return res.redirect('back');
+      }
+
+      const password = await bcrypt.hash(req.body.password, 5);
+
+      await User.create({ ...req.body, password });
+
+      req.flash('success', 'Usuário cadastrado com sucesso');
+      return res.redirect('/');
+    } catch (err) {
+      return next(err);
     }
-
-    const password = await bcrypt.hash(req.body.password, 5);
-
-    await User.create({ ...req.body, password });
-
-    req.flash('success', 'Usuário cadastrado com sucesso');
-    return res.redirect('/');
   },
 
-  async authenticate(req, res) {
-    const { email, password } = req.body;
+  async authenticate(req, res, next) {
+    try {
+      const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+      const user = await User.findOne({ where: { email } });
 
-    if (!user) {
-      req.flash('error', 'Usuário inexistente');
-      return res.redirect('back');
+      if (!user) {
+        req.flash('error', 'Usuário inexistente');
+        return res.redirect('back');
+      }
+
+      if (!(await bcrypt.compare(password, user.password))) {
+        req.flash('error', 'Senha incorreta');
+        return res.redirect('back');
+      }
+
+      req.session.user = user;
+
+      return req.session.save(() => res.redirect('app/dashboard'));
+    } catch (err) {
+      return next(err);
     }
+  },
 
-    if (!(await bcrypt.compare(password, user.password))) {
-      req.flash('error', 'Senha incorreta');
-      return res.redirect('back');
-    }
-
-    req.session.user = user;
-
-    return req.session.save(() => res.redirect('app/dashboard'));
+  signout(req, res) {
+    return req.session.destroy(() => {
+      res.redirect('/');
+    });
   },
 };
--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const routes = express.Router();
 
+const authMiddleware = require('./middlewares/auth');
+const guestMiddleware = require('./middlewares/guest');
+
 const authController = require('./controllers/authController');
+const dashboardController = require('./controllers/dashboardController');
 
 routes.use((req, res, next) => {
   res.locals.flashSuccess = req.flash('success');
@@ -10,10 +14,27 @@ routes.use((req, res, next) => {
   next();
 });
 
-routes.get('/', authController.signin);
-routes.get('/signup', authController.signup);
+routes.get('/', guestMiddleware, authController.signin);
+routes.get('/signup', guestMiddleware, authController.signup);
+routes.get('/signout', authController.signout);
 
 routes.post('/register', authController.register);
 routes.post('/authenticate', authController.authenticate);
+
+routes.use('/app', authMiddleware);
+routes.get('/app/dashboard', dashboardController.index);
+
+routes.use((err, res) => {
+  res.render('errors/404');
+});
+
+routes.use((err, req, res, _next) => {
+  res.status(err.status || 500);
+
+  return res.render('errors/index', {
+    message: err.message,
+    error: process.env.NODE_ENV === 'production' ? {} : err,
+  });
+});
 
 module.exports = routes;
